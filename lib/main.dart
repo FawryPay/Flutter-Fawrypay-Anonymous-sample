bf2ae60b1c217fe9d0c3e7bf39ede7ad384fe0ea
Milad Metias <milad.metias@fawry.com>
2024-01-21 17:36:37 +0200

    fix cred

--- lib/main.dart
+++ lib/main.dart
@@ -14,8 +14,8 @@ import 'package:fawry_sdk/model/payment_methods.dart';
 import 'package:fawry_sdk/model/response.dart';
 
 class Constants {
-  static const String merchantCode = "770000017893";
-  static const String secureKey = "0c96f509-d488-4a6b-b08a-a0d9782a6517";
+  static const String merchantCode = "+/IAAY2nothN6tNlekupwA==";
+  static const String secureKey = "4b815c12-891c-42ab-b8de-45bd6bd02c3d";
   static const String baseUrl = "https://atfawry.fawrystaging.com";
 }
 
